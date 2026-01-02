68d7e4de33d2de591cf65a5ca19b2b54dabc7a57
LDLPROJECT\sergii.osaulenko <sergii.osaulenko@arrise.com>
2026-01-02 11:56:33 +0200

    Initial Commit

---
+++ generate_arrays.py
@@ -0,0 +1,17 @@
+def generate_arrays(sizes, kind="random"):
+    arrays = {}
+    for n in sizes:
+        if kind == "random":
+            arrays[n] = [random.randint(0, 10**6) for _ in range(n)]
+        elif kind == "sorted":
+            arrays[n] = list(range(n))
+        elif kind == "reversed":
+            arrays[n] = list(range(n, 0, -1))
+        elif kind == "almost_sorted":
+            base = list(range(n))
+            # робимо кілька випадкових перестановок
+            for _ in range(max(1, n // 100)):
+                i, j = random.randrange(n), random.randrange(n)
+                base[i], base[j] = base[j], base[i]
+            arrays[n] = base
+    return arrays
